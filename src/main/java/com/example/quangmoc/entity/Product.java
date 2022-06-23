47e86da91f90da30d023f4139c43ec6e8610e177
HongQuang <nguyenhongquang2002@gmail.com>
2022-06-23 08:31:14 +0700

    chay ngay di

--- src/main/java/com/example/quangmoc/entity/Product.java
+++ src/main/java/com/example/quangmoc/entity/Product.java
@@ -16,7 +16,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)//
     private int id;
-        private String name ;
+    private String name ;
     private String slug ;
     private String thumbnail ;
     @Column(columnDefinition = "Text")
